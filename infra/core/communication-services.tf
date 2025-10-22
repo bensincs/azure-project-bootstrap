ffe1d9458027e3deb21dc6e09cc86ee8bf548030
bensincs <ben@sincs.co.uk>
2025-10-22 13:56:37 +0400

    Wrong rg

--- infra/core/communication-services.tf
+++ infra/core/communication-services.tf
@@ -1,7 +1,7 @@
 # Azure Communication Services for WebRTC TURN/STUN servers
 resource "azurerm_communication_service" "webrtc" {
   name                = "acs-${var.resource_name_prefix}-${var.environment}"
-  resource_group_name = azurerm_resource_group.main.name
+  resource_group_name = azurerm_resource_group.core.name
   data_location       = "United States" # Options: United States, Europe, Asia Pacific, United Kingdom, Australia
 
   tags = local.common_tags
